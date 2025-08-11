fd68cdba1882385947b5669bad7c5264490a32ab
eunju <ehdgmd12@naver.com>
2025-08-11 18:10:21 +0900

    js수정

--- script/weather.js
+++ script/weather.js
@@ -8,17 +8,4 @@ const cityEl = document.querySelector('.temp p');
 const tempEl = document.querySelector('.temp strong');
 
 btn.addEventListener('click', function () {
-    if (d.desc === '맑음') {
-        iconEl.src = './images/rain_icon.png';
-        heroEl.src = './images/rain_img.png';
-        descEl.textContent = '오늘은 맑아요. 가벼운 옷차림을 추천해요.';
-    } else if (d.desc === '흐림') {
-        iconEl.src = './images/rain_icon.png';
-        heroEl.src = './images/rain_img.png';
-        descEl.textContent = '구름이 많아요. 일교차에 유의하세요.';
-    } else {
-        iconEl.src = './images/rain_icon.png';
-        heroEl.src = './images/rain_img.png';
-        descEl.textContent = '오늘은 비가 내립니다. 우산을 챙겨주세요.';
-    }
 });
